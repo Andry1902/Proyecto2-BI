89d6f4ce04a3e5ab7ac4c53b41915fab759a0769
Dani-3800 <danielmoralesvalentino@gmail.com>
2026-02-17 13:46:53 -0400

    tables.sql

--- sql/tables.sql
+++ sql/tables.sql
@@ -9,32 +9,32 @@ SET search_path TO expoucv_g30030004;
 --Tipo evento
 CREATE TABLE TIPO_EVENTO (
 	cod_tipo_evento INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	nb_tipo_evento VARCHAR(100)
+	nb_tipo_evento VARCHAR(100) NOT NULL
 );
 
 --Categoria
 CREATE TABLE CATEGORIA (
 	cod_categoria INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	nb_categoria VARCHAR(100)
+	nb_categoria VARCHAR(100) NOT NULL
 );
 
 --Tipo stand
 CREATE TABLE TIPO_STAND (
 	cod_tipo_stand INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	nb_tipo_stand VARCHAR(100)
+	nb_tipo_stand VARCHAR(100) NOT NULL
 );
 
 --Pais
 CREATE TABLE PAIS (
 	cod_pais INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	nb_pais VARCHAR(100)
+	nb_pais VARCHAR(100) NOT NULL
 );
 
 --Cliente
 CREATE TABLE CLIENTE (
 	cod_cliente INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	nb_cliente VARCHAR(100),
-	ci_rif VARCHAR(20),
+	nb_cliente VARCHAR(100) NOT NULL,
+	ci_rif VARCHAR(20) NOT NULL,
 	telefono VARCHAR(20),
 	direccion VARCHAR(255),
 	email VARCHAR(100)
@@ -43,14 +43,14 @@ CREATE TABLE CLIENTE (
 --Leyenda estrellas
 CREATE TABLE LEYENDA_ESTRELLAS (
 	cod_leyenda_estrellas INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	nb_descripcion VARCHAR(100)
+	nb_descripcion VARCHAR(100) NOT NULL
 );
 
 --Visitante
 CREATE TABLE VISITANTE (
 	cod_visitante INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	cedula VARCHAR(20),
-	nb_visitante VARCHAR(100),
+	cedula VARCHAR(20) NOT NULL,
+	nb_visitante VARCHAR(100) NOT NULL,
 	sexo CHAR(1),
 	email VARCHAR(100)
 );
@@ -59,8 +59,8 @@ CREATE TABLE VISITANTE (
 --Subcategoria, tiene referencia a tabla categoria
 CREATE TABLE SUBCATEGORIA (
 	cod_sub_categoria INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	nb_sub_categoria VARCHAR(100),
-	cod_categoria INT,
+	nb_sub_categoria VARCHAR(100) NOT NULL,
+	cod_categoria INT NOT NULL,
 
 	CONSTRAINT fk_categoria
 		FOREIGN KEY (cod_categoria)
@@ -70,8 +70,8 @@ CREATE TABLE SUBCATEGORIA (
 --Ciudad, tiene refencia a tabla pais
 CREATE TABLE CIUDAD (
 	cod_ciudad INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	nb_ciudad VARCHAR (100),
-	cod_pais INT,
+	nb_ciudad VARCHAR (100) NOT NULL,
+	cod_pais INT NOT NULL,
 
 	CONSTRAINT fk_pais
 		FOREIGN KEY (cod_pais)
@@ -81,8 +81,8 @@ CREATE TABLE CIUDAD (
 --Sede, tiene referencia a tabla ciudad
 CREATE TABLE SEDE (
 	cod_sede INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	nb_sede VARCHAR(100),
-	cod_ciudad INT,
+	nb_sede VARCHAR(100) NOT NULL,
+	cod_ciudad INT NOT NULL,
 
 	CONSTRAINT fk_ciudad
 		FOREIGN KEY (cod_ciudad)
@@ -92,13 +92,13 @@ CREATE TABLE SEDE (
 --Evento, tiene refencia de tabla Tipo_evento y sede 
 CREATE TABLE EVENTO (
 	cod_evento INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	nb_evento VARCHAR(100),
-	fecha_inicia DATE,
-	fecha_fin DATE,
+	nb_evento VARCHAR(100) NOT NULL,
+	fecha_inicia DATE NOT NULL,
+	fecha_fin DATE NOT NULL,
 	descripcion VARCHAR(255),
-	cod_sede INT,
+	cod_sede INT NOT NULL,
 	email VARCHAR(100),
-	cod_tipo_evento INT,
+	cod_tipo_evento INT NOT NULL,
 
 	CONSTRAINT fk_sede
 		FOREIGN KEY (cod_sede)
@@ -111,13 +111,13 @@ CREATE TABLE EVENTO (
 --Contrato, tiene refencia de tabla cliente, subcategoria, evento, tipo stand
 CREATE TABLE CONTRATO (
 	nro_stand INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	cod_evento INT,
-	cod_tipo_stand INT,
-	fecha_alquiler DATE,
-	cod_cliente INT,
-	mts2 DECIMAL(10,2),
-	Monto DECIMAL(12,2),
-	cod_sub_categoria INT,
+	cod_evento INT NOT NULL,
+	cod_tipo_stand INT NOT NULL,
+	fecha_alquiler DATE NOT NULL,
+	cod_cliente INT NOT NULL,
+	mts2 DECIMAL(10,2) NOT NULL,
+	Monto DECIMAL(12,2) NOT NULL,
+	cod_sub_categoria INT NOT NULL,
 
 	CONSTRAINT fk_evento
 		FOREIGN KEY (cod_evento)
@@ -135,11 +135,11 @@ CREATE TABLE CONTRATO (
 
 --Evento Stand, tiene referencia de tabla tipo stand y evento
 CREATE TABLE EVENTO_ESTAND (
-	cod_evento INT,
-	cod_tipo_stand INT,
-	cantidad_estimada INT,
-	mts2 DECIMAL(10,2),
-	precio DECIMAL(12,2),
+	cod_evento INT NOT NULL,
+	cod_tipo_stand INT NOT NULL,
+	cantidad_estimada INT NOT NULL,
+	mts2 DECIMAL(10,2) NOT NULL,
+	precio DECIMAL(12,2) NOT NULL,
 
 	CONSTRAINT pk_evento_stand
 		GENERATED ALWAYS AS IDENTITY PRIMARY KEY (cod_evento, cod_tipo_stand),
@@ -154,13 +154,13 @@ CREATE TABLE EVENTO_ESTAND (
 --Entrada, tiene referencia de la tabla Leyenda_estrella, evento y visitante
 CREATE TABLE ENTRADA (
 	nro_entrada INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	cod_evento INT,
-	fecha_entrada DATE,
-	hora_entrada TIME,
-	cod_visitante INT,
-	recomienda_amigo INT,
-	calificacion INT,
-	cod_leyenda_estrellas INT,
+	cod_evento INT NOT NULL,
+	fecha_entrada DATE NOT NULL,
+	hora_entrada TIME NOT NULL,
+	cod_visitante INT NOT NULL,
+	recomienda_amigo INT NOT NULL,
+	calificacion INT NOT NULL,
+	cod_leyenda_estrellas INT NOT NULL,
 
 	CONSTRAINT fk_evento
 		FOREIGN KEY (cod_evento)
